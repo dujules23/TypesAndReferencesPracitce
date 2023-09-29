4ccebc9a13f3ef8f7e72008b40f84e9d24dd4648
Durrell Jules <dujules23@gmail.com>
2023-09-29 13:22:20 -0400

    first example learning about types and references

---
+++ TypesAndReferencePractice/Elephant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TypesAndReferencePractice
+{
+	public class Elephant
+	{
+		public string? Name;
+		public int EarSize;
+		public void WhoAmI()
+		{
+			Console.WriteLine("My Name is " + Name + ".");
+			Console.WriteLine("My Ears are " + EarSize + " inches tall.");
+		}
+
+	}
+}
+
---
+++ TypesAndReferencePractice/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TypesAndReferencePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33};
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("You pressed " + input);
+                if (input == '1')
+                {
+                    Console.WriteLine("Calling llyod.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (input == '2')
+                {
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (input == '3')
+                {
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                    Console.WriteLine("References have been swapped.");
+                }
+                else return;
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
